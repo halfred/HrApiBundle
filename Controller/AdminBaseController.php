d00c9d087d34b196352a870f0e62246e58d40075
halfred <halfred68@gmail.com>
2019-08-13 22:43:29 +0200

    improuv

--- Controller/AdminBaseController.php
+++ Controller/AdminBaseController.php
@@ -31,17 +31,17 @@ abstract class AdminBaseController extends BaseController
         $users = $userRepository->findAll();
 
         $response = $serializer->serialize($users, 'json');
-        return new Response($response);
+        return new Response($response, 200, ['Content-Type' => 'application/json']);
     }
 
     /**
      * Get one user by id
      * @param Request $request The http request
      * @param int $userId The user id
-     * @return JsonResponse
+     * @return Response
      * @Route("/{userId}", name="user_get_one", methods={"GET"})
      */
-    public function getOne(SerializerInterface $serializer, int $userId)
+    public function getOne(SerializerInterface $serializer, int $userId): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
         /** @var UserRepository $userRepository */
@@ -49,49 +49,20 @@ abstract class AdminBaseController extends BaseController
         $user = $userRepository->find($userId);
 
         $response = $serializer->serialize($user, 'json');
-        return new Response($response);
+        return new Response($response, 200, ['Content-Type' => 'application/json']);
     }
 
     /**
      * delete by Id
      * @param Request $request The http request
      * @param int $id The  id
-     * @return JsonResponse
+     * @return Response
      * @Route("/{id}", name="user_delete", methods={"delete"})
      */
-    public function delete(Request $request, int $id)
+    public function delete(Request $request, SerializerInterface $serializer, int $id)
     {
-        return $this->json([
-            'message' => 'delete ' . $id,
-        ]);
+        $response = $serializer->serialize($id, 'json');
+        return new Response($response, 200, ['Content-Type' => 'application/json']);
     }
-
-//    /**
-//     * create an user
-//     * @param Request $request The http request
-//     * @return JsonResponse
-//     * @Route("/", name="user_create", methods={"POST"})
-//     */
-//    public function create(Request $request)
-//    {
-//        return $this->json([
-//            'message' => 'create User',
-//        ]);
-//    }
-//
-//    /**
-//     * update an user
-//     * @param Request $request The http request
-//     * @param int     $userId  The user id
-//     * @return JsonResponse
-//     * @Route("/{userId}", name="user_update", methods={"put"})
-//     */
-//    public function update(Request $request, $userId)
-//    {
-//        return $this->json([
-//            'message' => 'update User ' . $userId,
-//        ]);
-//    }
-
 
 }
--- Controller/BaseController.php
+++ Controller/BaseController.php
@@ -3,11 +3,6 @@
 namespace Hr\ApiBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
-use Symfony\Component\Routing\Annotation\Route;
 
 
 /**
@@ -16,73 +11,26 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 abstract class BaseController extends Controller
 {
-//    /**
-//     * Get multiple
-//     * @param SerializerBuilder $serializerBuilder the serializer
-//     * @return Response
-//     * @Route("/", name="user_get_multi", methods={"GET"})
-//     */
-//    public function getMulti(SerializerBuilder $serializerBuilder)
-//    {
-//        return $this->json([
-//            'message' => "Get all",
-//        ]);
-//    }
-//
-//    /**
-//     * Get one user by id
-//     * @param Request $request The http request
-//     * @param int $userId The user id
-//     * @return JsonResponse
-//     * @Route("/{userId}", name="user_get_one", methods={"GET"}, requirements={"userId"="\d+"})
-//     */
-//    public function getOne(SerializerBuilder $serializerBuilder, int $userId)
-//    {
-//        return $this->json([
-//            'message' => "Get One $userId",
-//        ]);
-//    }
-//
-//    /**
-//     * delete by Id
-//     * @param Request $request The http request
-//     * @param int $id The  id
-//     * @return JsonResponse
-//     * @Route("/{id}", name="user_delete", methods={"delete"})
-//     */
-//    public function delete(Request $request, int $id)
-//    {
-//        return $this->json([
-//            'message' => 'delete ' . $id,
-//        ]);
-//    }
+    /**
+     * @var string
+     */
+    protected $defaultEntitySerializationGroup;
 
-//    /**
-//     * create an user
-//     * @param Request $request The http request
-//     * @return JsonResponse
-//     * @Route("/", name="user_create", methods={"POST"})
-//     */
-//    public function create(Request $request)
-//    {
-//        return $this->json([
-//            'message' => 'create User',
-//        ]);
-//    }
-//
-//    /**
-//     * update an user
-//     * @param Request $request The http request
-//     * @param int $userId The user id
-//     * @return JsonResponse
-//     * @Route("/{userId}", name="user_update", methods={"put"}, requirements={"userId"="\d+"})
-//     */
-//    public function update(Request $request, int $userId)
-//    {
-//        return $this->json([
-//            'message' => 'update User ' . $userId,
-//        ]);
-//    }
+    /**
+     * BaseController constructor.
+     */
+    public function __construct()
+    {
+        $this->setDefaultEntitySerializationGroup();
+    }
 
-
+    /**
+     *
+     */
+    protected function setDefaultEntitySerializationGroup(): void
+    {
+        $defaultEntitySerializationGroup = explode('\\', get_class($this));
+        $defaultEntitySerializationGroup = lcfirst(array_pop($defaultEntitySerializationGroup));
+        $this->defaultEntitySerializationGroup = str_replace('Controller', '', $defaultEntitySerializationGroup);
+    }
 }
