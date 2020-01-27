0d8164b202a0778fadfb17498124ddf575148cfb
halfred <halfred68@gmail.com>
2020-01-27 20:57:10 +0100

    test

--- Repository/UserRepository.php
+++ Repository/UserRepository.php
@@ -4,7 +4,7 @@ namespace Hr\ApiBundle\Repository;
 
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Hr\ApiBundle\Entity\User;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method User|null find($id, $lockMode = null, $lockVersion = null)
@@ -21,7 +21,7 @@ class UserRepository extends ServiceEntityRepository
         ],
     ];
 
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, User::class);
     }
